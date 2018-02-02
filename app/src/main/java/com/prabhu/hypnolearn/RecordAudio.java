85582ff453c9246f96f63439f58ea15d7d951cb3
prabhubalu <im.prabhub@gmail.com>
2018-02-02 23:31:03 +0530

    Custom File name and Directry storage

---
+++ app/src/main/java/com/prabhu/hypnolearn/RecordAudio.java
@@ -0,0 +1,13 @@
+package com.prabhu.hypnolearn;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RecordAudio extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.recordaudio_activity);
+    }
+}
