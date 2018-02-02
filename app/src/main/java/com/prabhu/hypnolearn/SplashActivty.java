ae389d3cfadd665692e80c7c0b658fb63cf3601c
prabhubalu <im.prabhub@gmail.com>
2018-02-02 13:21:22 +0530

    first commit

---
+++ app/src/main/java/com/prabhu/hypnolearn/SplashActivty.java
@@ -0,0 +1,13 @@
+package com.prabhu.hypnolearn;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivty extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_activty);
+    }
+}
